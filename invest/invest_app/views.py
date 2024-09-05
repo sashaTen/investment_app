aae4f94b8e078a2d2abe91cc47b7c6d580ba66e5
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-09-05 11:11:47 +0900

    added  the  hhome  template

--- invest/invest_app/views.py
+++ invest/invest_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sentiment(request):
-    return HttpResponse("Hello, World!")
+    return render(request , 'home.html')
