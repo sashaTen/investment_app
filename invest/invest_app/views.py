98394a7aed53ea876be8d37f61db43bcd545d1ed
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-09-05 11:37:42 +0900

    created   sentiment   analysis   model   and now   users    iniput  the  text  and  get the sentiment

--- invest/invest_app/views.py
+++ invest/invest_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import   joblib
 # Create your views here.
 from django.http import HttpResponse
 
@@ -7,5 +7,14 @@ def sentiment(request):
     return render(request , 'home.html')
 
 def  sentimentResult(request): 
-    text    =   request.POST['sentiment']
-    return   HttpResponse(text)
+    loaded_model = joblib.load('sentiment_model.pkl')
+    loaded_vectorizer = joblib.load('vectorizer.pkl')
+    sentiment = request.POST['sentiment']
+    sentiment =  [sentiment]
+    sentiment_vectorized = loaded_vectorizer.transform(sentiment)
+
+# Make a prediction
+    prediction = loaded_model.predict(sentiment_vectorized)
+
+# Output the prediction
+    return HttpResponse(f'Predicted sentiment: {prediction[0]}')
