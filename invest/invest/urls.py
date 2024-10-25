9bf34c4f4d318823316c0de125c4d84548f49d44
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-10-25 13:24:25 +0900

    created the oage   of  hello  world  for the asset   management    page

--- invest/invest/urls.py
+++ invest/invest/urls.py
@@ -21,4 +21,5 @@ from django.urls import path ,   include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' ,  include('invest_app.urls')),
+    path('assets',  include('portfolio.urls')),
 ]
--- invest/portfolio/views.py
+++ invest/portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+   
+    
+# Create a DataFrame from the list of dictionaries
+    #df = turn_database_into_dataframe(50)
+    
+def  portfolioHome(request):
+    return   HttpResponse('asset   management   page')
