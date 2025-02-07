3219a74f9a1b9df6b87f4a053b83180b9e79c526
TEN ALEXANDER <sasha1keshten@gmail.com>
2025-02-07 15:31:48 +0900

    just    rebuild the   inferenee   for the modmodel  liaded from the  zenml

--- invest/invest_app/views.py
+++ invest/invest_app/views.py
@@ -14,6 +14,13 @@ from .utilities2   import   turn_database_into_dataframe
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 
+vectorizer_artifact = Client().get_artifact_version("91734428-5393-42ff-a7c2-45973edd995d")
+vectorizer = vectorizer_artifact.load()
+
+
+model_artifact = Client().get_artifact_version("121c294e-3bba-40bb-9e11-a3f9d2dd42f5")
+model = model_artifact.load()
+
 def sentiment(request):
     return render(request , 'home.html')
 
@@ -45,47 +52,16 @@ def    testing(request):
 
 
 def  sentimentResult(request): 
-#  the   script  for   subproccess and the autoretrain  you   will  find  in   notes
-#  all  you  need  is   just  in   copy  paste  it  here     
-#  Load the latest model
-    model , vectorizer,  accuracy   = load_current_vectorizer_and_model()
-    tweet = request.POST['tweet']
-    sentiment  = request.POST['sentiment']
-    tweet =  [tweet]
-    sample_count =  Count_samples_for_retrain.objects.first()
-    tweet_db_length= TweetSentiment.objects.count()
-    tweet_vectorized =  vectorizer.transform(tweet)
-    prediction = model.predict(tweet_vectorized)
-    new_tweet = TweetSentiment(tweet_text=tweet, prediction=prediction , sentiment = sentiment)
-    new_tweet.save()
-    new_tweet_db_length = TweetSentiment.objects.count()
-    
-    if (new_tweet_db_length>tweet_db_length):
-        sample_count.samples_number= sample_count.samples_number+1
-        sample_count.save()
 
-    if (sample_count.samples_number % 500 == 0):
-        print(sample_count.samples_number,   '   updated' )
-        zen_sentiment_analysis_pipeline()
-        sample_count.samples_number = 0
-        sample_count.save()
-    
-    
-   
-    '''  
-    model , vectorizer,  accuracy   = load_current_vectorizer_and_model()
     tweet = request.POST['tweet']
-    sentiment  = request.POST['sentiment']
     tweet =  [tweet]
+    
     tweet_vectorized =  vectorizer.transform(tweet)
     prediction = model.predict(tweet_vectorized)
-    new_tweet = TweetSentiment(tweet_text=tweet, prediction=prediction , sentiment = sentiment)
-    new_tweet.save()
-    df =   turn_database_into_dataframe()
-    print(df.head())'''
-   
 # Output the prediction
-    return HttpResponse(f'samples  count   :   {sample_count.samples_number} ,  prediction is   :{prediction} ,   the  model is  : {model}' )
+
+    #return HttpResponse(f'samples  count   :   {sample_count.samples_number} ,  prediction is   :{prediction} ,   the  model is  : {model}' )
+    return HttpResponse(prediction)
     
 
 
